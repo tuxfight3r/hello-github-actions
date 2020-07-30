175058064be67c17a1a98b5529a8dde913861fba
Mohan Balasundaram <mohan@nerdplanet.co.uk>
2020-07-30 01:01:40 +0100

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo -e Hello world my name is $INPUT_MY_NAME \n SECRET1: $INPUT_SECRET1 \n SECRET2: $INPUT_SECRET2 \n"
+sh -c "echo Hello world my name is $INPUT_MY_NAME || SECRET1: $INPUT_SECRET1 || SECRET2: $INPUT_SECRET2"
